83f3c62e73d26610764d662b6f500310f5f8b484
Jani Hellström <jajahel@gmail.com>
2019-09-06 19:17:26 +0300

    CreateNewUser (rautalanka)
    index.html === Otin resetin pois, tein siihen tilalle signupin, joka vastannee käyttäjän tarpeeseen paremmin.
    
    index.js === Tein tänne ohjaukset uuteen lomakkeeseen (signup) sekä kaksi funktiota: toinen on apufunktio, joka luo uuden objektin käyttäjädatasta. Ns. pääfunktio hakee lomakedatan, callaa apufunktion, ja tallentaa käyttäjän välimuistiin.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-console.log("Hello again world!")
+
+
+
+
+
 // Aina kun index.html avataan, niin varmistetaan että sivu ymmärtää ettei kukaan ole vielä kirjautunut
 localStorage.setItem("loggedIn", false)
 localStorage.removeItem("userId")
@@ -16,3 +20,44 @@ function check(form) {
    alert("Error Password or Username") //displays error message
   }
 }
+
+
+/* Tied to "Sign up" button. Sends you to the sign-up form. */ 
+
+const signUpPage = () => {
+    window.open('signUpBoard.html')
+  };
+
+
+
+/* This function is tied to the eventListener on the signUpBoard form. It saves the data,
+and calls an object creator amidst doing that. Rautalankamalli, koska ei holdaa 
+kuin yhden käyttäjän toistaiseksi. Useampi käyttäjä vaatii datamallin ja classin methodeille. */
+
+const persistData = (form) => {
+  const uName = form.username.value;
+  const uPasw = form.password.value;
+  const item =  makeItem(uName, uPasw);
+  //console.log(item); for debugging purposes
+
+  //To make sure there is an item to persist
+  if (item) {
+    localStorage.setItem('userData', JSON.stringify(item));
+    
+  } else {
+    console.log(error)
+  }
+
+};
+
+/* Function that makes the user object. Separate function to keep
+our code clean and maintainable. */
+
+const makeItem = (name, passw) => {
+  const newItem = { 
+    name, 
+    passw: '/// TÄHÄN HASH OIKEASTI ///' + ' ' + passw, 
+    userID: Math.round(Math.random() * 10) //Tää vaan testinä. Pitäs integroida ulkonen kirjasto, mut en saanut sitä toimii oikkien takia.
+  }
+  return newItem;
+};
