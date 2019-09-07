313bf7303e8eb43efcd992a4dae1ec51a2f8364d
Sasu Paakkunainen <sasupa@gmail.com>
2019-09-08 01:25:25 +0300

    API testailua

--- js/apiTest.js
+++ js/apiTest.js
@@ -1,2 +1,11 @@
 // Täällä pitäs testailla että miten saadaan vaikka ensin vaan jostain
 // APIsta sataa konsoliin
+
+gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: 1CWVgJTW-CYM8ly940mzskTeoyPfXY2_FTLzn4537Wy8,
+    range: Sheet1!A1:B2
+  }).then((response) => {
+    var result = response.result;
+    var numRows = result.values ? result.values.length : 0;
+    console.log(`${numRows} rows retrieved.`);
+  });
