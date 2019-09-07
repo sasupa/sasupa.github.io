d53cdc59387c2fcb6f1dba5383ca199c0fcec6e2
Sasu Paakkunainen <sasupa@gmail.com>
2019-09-08 01:28:14 +0300

    Update apiTest.js

--- js/apiTest.js
+++ js/apiTest.js
@@ -2,9 +2,9 @@
 // APIsta sataa konsoliin
 
 gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: 1CWVgJTW-CYM8ly940mzskTeoyPfXY2_FTLzn4537Wy8,
-    range: Sheet1!A1:B2
-  }).then((response) => {
+    spreadsheetId: "1CWVgJTW-CYM8ly940mzskTeoyPfXY2_FTLzn4537Wy8",
+    range: "Sheet1!A1:B2"
+    }).then((response) => {
     var result = response.result;
     var numRows = result.values ? result.values.length : 0;
     console.log(`${numRows} rows retrieved.`);
