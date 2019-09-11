aea8bedef1239b99ec38b6c5af5037a8aad1495d
Sasu Paakkunainen <sasupa@gmail.com>
2019-09-11 13:44:03 +0300

    Added package, procfile and run.js

---
+++ run.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+const PORT = process.env.PORT || 5000;
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    if (filename == "./") {filename = "./index"}
+
+    filename = filename + ".html";
+
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, {'content-type':'text/html'});
+            return res.end("404 Not found");
+        }
+        res.writeHead(200, {'content-type':'text/html'});
+        res.write(data);
+        console.log("Incoming request... " + req.url);
+        res.end();
+    })
+}).listen(PORT);
+
+console.log("Server listening on port 8080...")
