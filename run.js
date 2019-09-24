a19518851d5b31abe1b76651b5c1120daa10a05b
Sasu Paakkunainen <sasupa@gmail.com>
2019-09-24 03:53:18 +0300

    partial path muutettu koska heroku crashaa

--- run.js
+++ run.js
@@ -8,9 +8,9 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, './public')
-const viewsPath = path.join(__dirname, './templates/views')
-const partialsPath = path.join(__dirname, './templates/partials')
+const publicDirectoryPath = path.join(__dirname, '/public')
+const viewsPath = path.join(__dirname, '/templates/views')
+const partialsPath = path.join(__dirname, '/templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
