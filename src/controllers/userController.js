ac45c8e334e341295d7e86fb15308009a3389652
Jani Hellström <jajahel@gmail.com>
2019-10-11 03:42:19 +0300

    Login enabled
    Login route, login function and verification with JWT Token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,17 +3,4 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
 
-
-exports.signUp = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
-    // await newUser.save();
-
-    res.status(201).json({
-        status: 'success',
-        data: {
-            user: newUser
-        }
-    });
-
-
-});
+// TÄNNE SITTEN NIITÄ ROUTEJA, JOTKA EI MEE AUTENTIKOINNIN PIIRIIN: UPDATE ME, DELETE ME ETC. 
--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 
 router.post('/signUp', authController.signup)
+router.post('/login', authController.login)
 
 module.exports = router
