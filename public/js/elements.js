0602e567543b75b97f593d7c7c562603b0fc7234
Sasu Paakkunainen <sasupa@gmail.com>
2019-10-12 16:00:15 +0300

    Login funktio toimii, elementit korjattu
    elements.js: kommentoin pois console.log testit, index.js: poistettiin vanha login toiminnallisuus ja korjattiin uudella ja asennettiin axios

--- public/js/elements.js
+++ public/js/elements.js
@@ -4,9 +4,9 @@ const inputPassword = document.getElementById("inputPassword")
 const rememberMe = document.getElementById("rememberMe")
 
 // Tämä testaa vain että inputit login-sivun formista tulee oikein
-signIn.addEventListener("submit", e => {
-    e.preventDefault()
-    console.log(inputEmail.value)
-    console.log(inputPassword.value)
-    console.log(rememberMe.checked)
-})
+// signIn.addEventListener("submit", e => {
+//     e.preventDefault()
+//     console.log(inputEmail.value)
+//     console.log(inputPassword.value)
+//     console.log(rememberMe.checked)
+// })
--- public/js/index.js
+++ public/js/index.js
@@ -1,103 +1,147 @@
-// Aina kun index.html avataan, niin varmistetaan että sivu ymmärtää ettei kukaan ole vielä kirjautunut
-localStorage.setItem("loggedIn", false)
-localStorage.removeItem("userId")
+// Login funktio
 
-// Tää funktio tarkastaa login sivulle syötetyt arvot
-function check(form) {
- if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd")
-  {
-    localStorage.setItem("loggedIn", true) // kun käyttäjätunnus ja salasana on oikein, jätetään tieto että on kirjautunut sisään
-    localStorage.setItem("userId", form.userid.value)
-    window.open('dashboard', "_self") //opens the target page while Id & password matches
+signIn.addEventListener("submit", e => {
+    e.preventDefault()
+    const email = inputEmail.value
+    const password = inputPassword.value
+    login(email, password)
+})
+
+const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'post',
+      url: '/users/login',
+      data: {
+        email,
+        password,
+
+      }
+  });
+  
+  if (res.data.status === "success") {
+    alert("Logged in successfully!");
+    window.setTimeout(() => {
+      location.assign("/dashboard");
+    }, 1500);
   }
- else
- {
-   alert("Error Password or Username") //displays error message
+
+  }
+  catch (err) {
+    alert(err.response.data.message)
   }
 }
 
+// Vanhoja funktioita, voi katsoa mallia joihinkin osiin
 
-/* Tied to "Sign up" button. Sends you to the sign-up form. */ 
+// signIn.addEventListener("submit", e => {
+//     e.preventDefault()
+//     console.log(inputEmail.value)
+//     console.log(inputPassword.value)
+//     console.log(rememberMe.checked)
 
-const signUpPage = () => {
-    window.open('signUpBoard')
-  };
+//     axios({
+//       method: 'post',
+//       url: '/users/login',
+//       data: {
+//         email: inputEmail.value,
+//         password: inputPassword.value
+//       }}).then((data) => {
+//       console.log(data.status)
+//       console.log(data)
+//     });
+// })
 
+//  if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd")
+//   {
+//     localStorage.setItem("loggedIn", true) // kun käyttäjätunnus ja salasana on oikein, jätetään tieto että on kirjautunut sisään
+//     localStorage.setItem("userId", form.userid.value)
+//     window.open('dashboard', "_self") //opens the target page while Id & password matches
+//   }
+//  else
+//  {
+//    alert("Error Password or Username") //displays error message
+//   }
 
+// /* Tied to "Sign up" button. Sends you to the sign-up form. */ 
 
-/* This function is tied to the eventListener on the signUpBoard form. It saves the data,
-and calls an object creator amidst doing that. Rautalankamalli, koska ei holdaa 
-kuin yhden käyttäjän toistaiseksi. Useampi käyttäjä vaatii datamallin ja classin methodeille. */
+// const signUpPage = () => {
+//     window.open('signUpBoard')
+//   };
 
-const persistData = (form) => {
-  const uName = form.username.value;
-  const uPasw = form.password.value;
-  const item =  makeItem(uName, uPasw);
-  //console.log(item); for debugging purposes
+// /* This function is tied to the eventListener on the signUpBoard form. It saves the data,
+// and calls an object creator amidst doing that. Rautalankamalli, koska ei holdaa 
+// kuin yhden käyttäjän toistaiseksi. Useampi käyttäjä vaatii datamallin ja classin methodeille. */
 
-  //To make sure there is an item to persist
-  if (item) {
-    localStorage.setItem('userData', JSON.stringify(item));
+// const persistData = (form) => {
+//   const uName = form.username.value;
+//   const uPasw = form.password.value;
+//   const item =  makeItem(uName, uPasw);
+//   //console.log(item); for debugging purposes
+
+//   //To make sure there is an item to persist
+//   if (item) {
+//     localStorage.setItem('userData', JSON.stringify(item));
     
-  } else {
-    console.log(error)
-  }
+//   } else {
+//     console.log(error)
+//   }
 
-};
+// };
 
-/* Function that makes the user object. Separate function to keep
-our code clean and maintainable. */
+// /* Function that makes the user object. Separate function to keep
+// our code clean and maintainable. */
 
-const makeItem = (name, passw) => {
-  const newItem = { 
-    userId: name, 
-    passw: '/// TÄHÄN HASH OIKEASTI ///' + ' ' + passw, 
-    number: Math.round(Math.random() * 10) //Tää vaan testinä. Pitäs integroida ulkonen kirjasto, mut en saanut sitä toimii oikkien takia.
-  }
-  return newItem;
-};
-
-
-/* Clear the fields after you click the button to show something is happening after the click. Works now only with signUp page
-because elements are named with "username" & password instead of "userid" or "pswrd" */
-
-const clearForm = (form) => {
-  form.username.value = '';
-  form.password.value = '';
-};
+// const makeItem = (name, passw) => {
+//   const newItem = { 
+//     userId: name, 
+//     passw: '/// TÄHÄN HASH OIKEASTI ///' + ' ' + passw, 
+//     number: Math.round(Math.random() * 10) //Tää vaan testinä. Pitäs integroida ulkonen kirjasto, mut en saanut sitä toimii oikkien takia.
+//   }
+//   return newItem;
+// };
 
 
-/* EVENT LISTENER: Siirtyminen signup-sivulta dashboard-näkymään. Koska koodi on
-on purkkapaikkaa ja patenttia, on vaikea siirtää tän funktion userID:tä dashin 
-näkymään niin, että se tunnistais uuden käyttäjän ja piirtäis muuna kuin "Nyt kirjautuneena: undefined".
+// /* Clear the fields after you click the button to show something is happening after the click. Works now only with signUp page
+// because elements are named with "username" & password instead of "userid" or "pswrd" */
 
-Ehdotus:
-1) Luodaan views-filet, jossa generoidaan HTML:ää ja manipuloidaan domia koordinoidusti. Esim
-Dashboardin näkymä oikeilla tiedoilla. Sit eventlistenerit controlleriin (index.js), ei HTML:ään kuten nyt (onclick).
-
-2) Hajautetaan CSS ja DOM-elementit omiin filuihin. VRT. se Base-file harkkaprojektissa.
-
-3) Opetellaan toi DOM-elementtien nimeäminen. Niitä voi kontrolloida monella tapaa: ID:llä, nimellä, tai
-classilla. En tiedä mikä on paras. Itse käytän aina classia.
-
-*/
+// const clearForm = (form) => {
+//   form.username.value = '';
+//   form.password.value = '';
+// };
 
 
-const signUpBtn = document.querySelector('.kliksautus');
+// /* EVENT LISTENER: Siirtyminen signup-sivulta dashboard-näkymään. Koska koodi on
+// on purkkapaikkaa ja patenttia, on vaikea siirtää tän funktion userID:tä dashin 
+// näkymään niin, että se tunnistais uuden käyttäjän ja piirtäis muuna kuin "Nyt kirjautuneena: undefined".
 
-if (signUpBtn) {
+// Ehdotus:
+// 1) Luodaan views-filet, jossa generoidaan HTML:ää ja manipuloidaan domia koordinoidusti. Esim
+// Dashboardin näkymä oikeilla tiedoilla. Sit eventlistenerit controlleriin (index.js), ei HTML:ään kuten nyt (onclick).
+
+// 2) Hajautetaan CSS ja DOM-elementit omiin filuihin. VRT. se Base-file harkkaprojektissa.
+
+// 3) Opetellaan toi DOM-elementtien nimeäminen. Niitä voi kontrolloida monella tapaa: ID:llä, nimellä, tai
+// classilla. En tiedä mikä on paras. Itse käytän aina classia.
+
+// */
 
 
-  signUpBtn.addEventListener('click', e => {
-    const userId = document.querySelector('.nimi').value;
+// const signUpBtn = document.querySelector('.kliksautus');
+
+// if (signUpBtn) {
+
+
+//   signUpBtn.addEventListener('click', e => {
+//     const userId = document.querySelector('.nimi').value;
    
    
-    localStorage.setItem("loggedIn", true) // kun käyttäjätunnus ja salasana on oikein, jätetään tieto että on kirjautunut sisään
-    localStorage.setItem("userId", userId)
-    window.open('dashboard') // Tähän pitäs jotenkin saada syötettyä kama, et DOMiin menis uuden userin tiedot; localStorageen ne tallentuu oikein.
+//     localStorage.setItem("loggedIn", true) // kun käyttäjätunnus ja salasana on oikein, jätetään tieto että on kirjautunut sisään
+//     localStorage.setItem("userId", userId)
+//     window.open('dashboard') // Tähän pitäs jotenkin saada syötettyä kama, et DOMiin menis uuden userin tiedot; localStorageen ne tallentuu oikein.
     
   
 
-    console.log(localStorage);
-  })};
+//     console.log(localStorage);
+//   })};
 
